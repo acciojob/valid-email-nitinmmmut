70972aafa2c8d0472c844bd9e32e55885f6a07df
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-03-24 13:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function validEmail(str) {
   //your JS code here.
+	function validEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+  //your JS code 
 }
 
 // Do not change the code below.
